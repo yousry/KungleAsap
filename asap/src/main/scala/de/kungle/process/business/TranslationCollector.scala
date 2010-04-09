5395d3227565c356fda5c1fe9feb806064a14a5e
Yousry Abdallah <mail@yousry.de>
2010-04-09 10:48:34 +0200

    Immediate Bot Respawn implemented.

--- asap/src/main/scala/de/kungle/process/business/TranslationCollector.scala
+++ asap/src/main/scala/de/kungle/process/business/TranslationCollector.scala
@@ -6,10 +6,12 @@ import http._
 import util._
 import Helpers._
 import net.liftweb.common._
+import net.liftweb.mapper._
+import de.kungle.asap.model.Wave
 
 object TranslationCollector extends LiftActor with BackgroundTask with Loggable {
   
-   override var scheduleIntervall : Long = 1 // 10 Minutes intervall
+   override var scheduleIntervall : Long = 10 // 10 Minutes intervall
        
    protected def messageHandler = {
       case DoWork => work
@@ -17,9 +19,23 @@ object TranslationCollector extends LiftActor with BackgroundTask with Loggable 
     }
 
     def work() = {
-   logger.info("TranslatorCollector Started at: %s".format(now))
-   logger.info("Translator finished.")
-    ActorPing.schedule(InfoCollector, InfoCollector.DoWork, scheduleIntervall minutes)
+      logger.info("TranslatorCollector Started at: %s".format(now))
+   
+      val untranslatedWaves = Wave.findAll(By(Wave.translator,"none"))
+      logger.info("Waves to translate: " + untranslatedWaves.length)
+
+      untranslatedWaves.foreach(new WaveTranslator(_))
+   
+      logger.info("Translator finished.")
+      
+      val stillWork = if(Wave.findAll(By(Wave.translator,"none")).length > 0) true else false
+      
+      if(stillWork) {
+        logger.info("Translator: still work to be done - continue in 10 seconds.")
+        ActorPing.schedule(TranslationCollector, TranslationCollector.DoWork, 10 seconds)
+      } else {
+        ActorPing.schedule(TranslationCollector, TranslationCollector.DoWork, scheduleIntervall minutes)
+      }
  }
 
   
