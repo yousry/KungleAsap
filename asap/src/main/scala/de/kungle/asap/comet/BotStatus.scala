c813838a554a1c9856e4692511b9ed12fa6523e9
Yousry Abdallah <mail@yousry.de>
2010-04-09 15:35:37 +0200

    First Idea for Comment field.

--- asap/src/main/scala/de/kungle/asap/comet/BotStatus.scala
+++ asap/src/main/scala/de/kungle/asap/comet/BotStatus.scala
@@ -1,9 +1,12 @@
 package de.kungle.asap.comet
 
+import scala.xml._
+
 import net.liftweb.common._
 import net.liftweb.http._
 import net.liftweb.http.js._
 import net.liftweb.http.js.JsCmds._
+
 
 import de.kungle.process.ProcessMaster
 
@@ -13,16 +16,25 @@ class BotStatus extends CometActor with Loggable{
   override def defaultPrefix = Full("bot")
 
   override def render = bind("status" -> rndStat)
+ 
+  var latestBots = List[(String, java.util.Date)]()
   
+  def rndStat = <span id="status">{plotBots}</span>
+    
+    def plotBots : NodeSeq = if(latestBots.length == 0) {
+      Text("Waiting for ProcessMaster sync.")
+    } else {
+      <table>
+      <tr><th>Bot</th><th>Last Execution</th></tr>
+      {latestBots.map(x => <tr><td>{x._1}</td><td>{x._2}</td></tr>  )}
+      </table>
+    }
   
-  
-  def rndStat = <span id="status">Wait for Sig</span>
-  
-
   override def lowPriority : PartialFunction[Any, Unit] = {
     case BotUpdateMessage(bots) => {
       logger.info("Stats Client message received from ProcessMaster." )
-      partialUpdate(SetHtml("status", <span id="status">{bots}</span>))
+      latestBots = bots
+      partialUpdate(SetHtml("status", <span id="status">{plotBots}</span>))
     }
     
   }
