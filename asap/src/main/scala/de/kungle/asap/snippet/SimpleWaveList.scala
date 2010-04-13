5a0c7492b06c5ac259b261779fe3b853028263d5
Yousry Abdallah <mail@yousry.de>
2010-04-13 19:31:04 +0200

    Ajax reload via button done.

--- asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
+++ asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
@@ -1,17 +1,44 @@
 package de.kungle.asap.snippet
 
+import scala.xml._
+
+import net.liftweb.common._
+
 import _root_.scala.xml.NodeSeq
 import _root_.net.liftweb.util.Helpers
 import Helpers._
 import _root_.net.liftweb.mapper._
+import net.liftweb.http.SHtml._
+import net.liftweb.http.js.JE._
+import net.liftweb.http.js.JsCmds._
+
 import de.kungle.asap.model._
 
 
-class SimpleWaveList {
+class SimpleWaveList extends Loggable {
 
+  def blubber : NodeSeq = {
+    
+    def renderEntry(w: Wave) = <div class="scroll-content-item ui-widget-content"> 
+      <b>English: </b>{w.title_english}<br /> 
+      <b>French: </b>{w.title_french}<br /> 
+      <b>German: </b>{w.title_german}<br /><br /> 
+      <b>English: </b>{w.summary_english}<br /> 
+      <b>French: </b>{w.summary_french}<br /> 
+      <b>German: </b>{w.summary_french}<br /><br /> 
+     </div>
+    
+     Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(25)).flatMap(renderEntry)
+    
+  }
+  
+  def queryReload = {
+    ajaxButton(Text("Press Me"),{() => 
+      logger.info("Press Me Pressed")
+      SetHtml("check-div", blubber )})
+  }
 
     def queryTabel(in: NodeSeq): NodeSeq = {
-
       
         def renderEntry(w: Wave): NodeSeq = bind("entry", chooseTemplate("query", "entries", in),
                 "titleEnglish" -> w.title_english,
@@ -22,12 +49,11 @@ class SimpleWaveList {
                 "summaryGerman" -> w.summary_german
         )
   
-  val entries = Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(50)).flatMap(renderEntry)
+  val entries = Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(25)).flatMap(renderEntry)
       
-      bind("query", in, "entries" -> entries)
+  bind("query", in, 
+       "entries" -> entries,
+       "button" -> queryReload)
       
     }
-
-
-  
 }
