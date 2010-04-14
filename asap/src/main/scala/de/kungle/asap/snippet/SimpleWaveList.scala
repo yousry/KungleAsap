808518cbf4ed459bc1887ccd76bdcacd57828d54
Yousry Abdallah <mail@yousry.de>
2010-04-14 13:46:06 +0200

    First implementation done.

--- asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
+++ asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
@@ -8,34 +8,46 @@ import _root_.scala.xml.NodeSeq
 import _root_.net.liftweb.util.Helpers
 import Helpers._
 import _root_.net.liftweb.mapper._
+import net.liftweb.http.S._
 import net.liftweb.http.SHtml._
 import net.liftweb.http.js.JE._
 import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.SessionVar
+
 
 import de.kungle.asap.model._
 
 
 class SimpleWaveList extends Loggable {
-
-  def smartScrollbar = JsRaw("""
- $('#target').scroll(function () { 
   
+
+  private var blubberCount = 0
+  
+  
+  
+  def ajaxBlubber(func: () => JsCmd, attrs: (String, String)*): Elem = {
+    attrs.foldLeft(fmapFunc(contextFuncBuilder(func))(name =>
+<script type="text/javascript">{JsRaw("""
+$('#target').scroll(function () { 
   var containerHeight = $('#target').height();
   var scroll = $('#target').scrollTop();
   var inners = $('#innerself').height();
-  
   var result = "ok"
   
-  // this should generate an ajax call
   if( containerHeight + scroll >= inners ) {
-   result = "false"
+   result = "false"; 
+
+   window.setTimeout(function () { """ + {makeAjaxCall(Str(name + "=true")).toJsCmd + "; return false;"} + """ }, 500);
+
   };
-  
   $('#alles').attr({value: containerHeight + '/' + scroll + '/' + inners +' ' + result});
- });  
-""")
+ });""")}</script>))(_ % _)
+  }
   
   def blubber : NodeSeq = {
+    
+    logger.info("Blubber Refresh called for page: " + blubberCount)
     
     def renderEntry(w: Wave) = <div class="scroll-content-item ui-widget-content"> 
       <b>English: </b>{w.title_english}<br /> 
@@ -46,14 +58,11 @@ class SimpleWaveList extends Loggable {
       <b>German: </b>{w.summary_german}<br /><br /> 
      </div>
     
-     Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(25)).flatMap(renderEntry)
-    
-  }
-  
-  def queryReload = {
-    ajaxButton(Text("Press Me"),{() => 
-      logger.info("Press Me Pressed")
-      SetHtml("check-div", blubber )})
+    var actualBlubber = blubberCount + 1
+    val newDiv = "check-div" + actualBlubber
+    blubberCount = actualBlubber
+
+    Wave.findAll(OrderBy(Wave.id, Descending ), StartAt(25 * actualBlubber), MaxRows(25)).flatMap(renderEntry) ++ <div id={newDiv}>actualBlubber</div>
   }
 
     def queryTabel(in: NodeSeq): NodeSeq = {
@@ -68,10 +77,12 @@ class SimpleWaveList extends Loggable {
         )
   
   val entries = Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(25)).flatMap(renderEntry)
-      
+  
+  
   bind("query", in, 
        "entries" -> entries,
-       "button" -> queryReload,
-       "smartScroll" ->  <script type="text/javascript">{smartScrollbar}</script> )
-    }
+       "smartScroll" -> ajaxBlubber({() => SetHtml("check-div" + {if(blubberCount == 0)"" else  blubberCount}, blubber )})  
+  )
+    
+  }
 }
