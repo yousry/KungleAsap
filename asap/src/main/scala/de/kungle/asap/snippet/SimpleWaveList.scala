fd59288d557cd22e36ae55fed7301879176a3fb8
Yousry Abdallah <mail@yousry.de>
2010-04-20 09:40:10 +0200

    ajax draggableUpdate with the flaw of a wave requery.

--- asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
+++ asap/src/main/scala/de/kungle/asap/snippet/SimpleWaveList.scala
@@ -20,7 +20,7 @@ import de.kungle.asap.model._
 
 
 class SimpleWaveList extends Loggable {
-
+  
   // for infinite scroll a imagetive page is calulated.
   private var pageCount = 0
   private var language = "original" // french, german
@@ -39,55 +39,68 @@ class SimpleWaveList extends Loggable {
   if( containerHeight + scroll + 50 >= inners ) {
    window.setTimeout(function () { """ + {makeAjaxCall(Str(name + "=true")).toJsCmd + "; return false;"} + """ }, 500);
    window.setTimeout(function () { """ + {makeAjaxCall(Str((ajaxInvoke(() => langScrollUpdate))._1 + "=true")).toJsCmd + "; return false;"} + """ }, 510);
+   window.setTimeout(function () { """ + {makeAjaxCall(Str((ajaxInvoke(() => draggableUpdate(Wave.findAll(OrderBy(Wave.id, Descending ), StartAt(25 * pageCount), MaxRows(25)))))._1 + "=true")).toJsCmd + "; return false;"} + """ }, 520);
   };
  });""")}</script>))(_ % _)
   }
 
+  
+        
+  def renderPic(w: Wave) : NodeSeq = {
+    val picID = "pic_" + w.id.is 
+    <div class="span-4" id={picID} >
+      <img src="/gfx/thumbnails/text3592.png" alt="PROCESS DATA" />
+    </div> 
+  }
+        
+  def renderTitle(lang: String, w: Wave)= lang match {
+    case "french" => <a href={w.url.is}>{w.title_french}</a>;
+    case "german" => <a href={w.url.is}>{w.title_german}</a>;
+    case _ => <a href={w.url.is}>{w.title_english}</a>;
+  }
+
+  def dragImgs(ws: List[Wave]) : JsRaw = {
+    def sigh(w: Wave) : String = "$(\"#pic_"+ w.id.is +"\").draggable({revert: true, helper: 'clone', appendTo: 'body', opacity: 0.4, tolerance: 'touch'});"  //  "$(\"#pic_" + w.id.is + "\").draggable({ revert: true, appendTo: 'body', opacity: 0.5});"
+    val v = ws.map{ s => sigh(s)}.mkString
+    JsRaw("$(function() {"  +{v}+ "});") 
+  }
+  
   // The server reaction by the scrollbar-end-event is to load an 
   // additional page and add it to the page. 
   def pageAppend : NodeSeq = {
     
     logger.info("PageAppend called for page: " + pageCount)
     
-    def renderEntry(w: Wave) = <div class="scroll-content-item ui-widget-content"> 
-      <keng><b>English: </b>{w.title_english}<br /><br /></keng> 
-      <kfrn><b>French: </b>{w.title_french}<br /><br /></kfrn> 
-      <kger><b>German: </b>{w.title_german}<br /><br /></kger> 
-      <keng><b>English: </b>{w.summary_english}<br /></keng> 
-      <kfrn><b>French: </b>{w.summary_french}<br /></kfrn> 
-      <kger><b>German: </b>{w.summary_german}<br /></kger>
-     </div>
+    def renderEntry(w: Wave) = <div class="scroll-content-item ui-widget-content">
+      {renderPic(w)}
+      <div class="span-22 last">
+        <keng>{renderTitle("english", w)}</keng>
+        <kfrn>{renderTitle("french", w)}</kfrn>
+        <kger>{renderTitle("german", w)}</kger>
+        <br/>
+        Publisher: <b>{w.publisher}</b><br/>
+        {w.publishingDate}<hr/>
+      </div>
+      <div class="summary span-26 last">   
+        <keng>{w.summary_english}<br /></keng> 
+        <kfrn>{w.summary_french}<br /></kfrn> 
+        <kger>{w.summary_german}<br /></kger>
+      </div>
+    </div> 
     
     var nextPage = pageCount + 1
     val newDiv = "check-div" + nextPage
     pageCount = nextPage
-
-    Wave.findAll(OrderBy(Wave.id, Descending ), StartAt(25 * nextPage), MaxRows(25)).flatMap(renderEntry) ++ <div id={newDiv}>Loading Data...</div>
+    
+    val waves = Wave.findAll(OrderBy(Wave.id, Descending ), StartAt(25 * nextPage), MaxRows(25))
+    
+    waves.flatMap(renderEntry) ++ 
+      <div id={newDiv}>Loading Data...</div> 
   }
 
     def queryTabel(in: NodeSeq): NodeSeq = {
-      
-      def renderPic(w: Wave) : NodeSeq = {
-        val picID = "pic_" + w.id.is 
-        <div class="span-4" id={picID} >
-                <img src="/gfx/thumbnails/text3592.png" alt="PROCESS DATA" />
-        </div> 
-      }
-      
-      def renderTitle(lang: String, w: Wave)= lang match {
-        case "french" => <a href={w.url.is}>{w.title_french}</a>;
-        case "german" => <a href={w.url.is}>{w.title_german}</a>;
-        case _ => <a href={w.url.is}>{w.title_english}</a>;
-      }
-      
 
         val waves = Wave.findAll(OrderBy(Wave.id, Descending ), MaxRows(25))
-
-        def dragImgs : JsRaw = {
-          def sigh(w: Wave) : String = "$(\"#pic_"+ w.id.is +"\").draggable({revert: true, helper: 'clone', appendTo: 'body', opacity: 0.4, tolerance: 'touch'});"  //  "$(\"#pic_" + w.id.is + "\").draggable({ revert: true, appendTo: 'body', opacity: 0.5});"
-          val v = waves.map{ s => sigh(s)}.mkString
-          JsRaw("$(function() {"  +{v}+ "});") 
-        }
         
         def renderEntry(w: Wave): NodeSeq = bind("entry", chooseTemplate("query", "entries", in),
                 "titleEnglish" -> {renderTitle("english", w)},
@@ -106,11 +119,18 @@ class SimpleWaveList extends Loggable {
   bind("query", in, 
        "entries" -> entries,
        "smartScroll" -> ajaxPageAppend({() => SetHtml("check-div" + {pageCount}, pageAppend )}),
-       "dragImgs" -> <script type="text/javascript">{dragImgs}</script> 
+       "dragImgs" -> <script type="text/javascript">{dragImgs(waves)}</script> 
   )
     
   }
 
+    
+    
+  def draggableUpdate(ws: List[Wave]) : JsCmd = {
+    logger.info("Draggable Update called.")
+    dragImgs(ws)
+  } 
+    
   def langScrollUpdate : JsCmd = {
     
     logger.info("LangScrollUpdate called for page: " + pageCount + " with language " + language)
