639740ac8d22f91b2b555d39cf4a885b091576ff
Yousry Abdallah <mail@yousry.de>
2010-05-23 15:47:02 +0200

    meanwhile

--- asap/src/main/scala/de/kungle/asap/snippet/WhatHappensNext.scala
+++ asap/src/main/scala/de/kungle/asap/snippet/WhatHappensNext.scala
@@ -1,10 +1,59 @@
 package de.kungle.asap.snippet
 
+import scala.xml._
+
+import net.liftweb.common._
+
 import _root_.scala.xml.NodeSeq
 import _root_.net.liftweb.util.Helpers
 import Helpers._
-import model._
+import _root_.net.liftweb.mapper._
+import net.liftweb.http.jquery.JqSHtml
+import net.liftweb.http.RequestVar
+import net.liftweb.http.S._
+import net.liftweb.http.SHtml
+import net.liftweb.http.SHtml._
+import net.liftweb.http.js.JE._
+import net.liftweb.http.js.JsCmds._
+import net.liftweb.http.js.{JsCmd,JsExp}
+import net.liftweb.http.SessionVar
 
-class WhatHappensNext {
+import _root_.de.kungle.asap.model.User
 
+import de.kungle.asap.model.Comment
+
+class WhatHappensNext extends Loggable {
+
+      
+  private var nana : String = ""
+  
+  private def storePrediction = {
+    
+    val comment = Comment.create
+    
+    val user = User.currentUser 
+    
+    ()
+    
+  }
+  
+  def comment(environs: NodeSeq) : NodeSeq = { 
+    
+    val blargh : () => JsCmd = 
+      () => {logger.info("New Prediction: " + nana)
+             storePrediction
+             JsRaw("$(\"#commentText\").val('');")
+      }
+    
+    val blub = Helpers.bind(
+    "preditction", environs, "cmnt" -> textarea("Type your message here!", 
+                                                gaga => {nana = gaga}) % ("id" -> "commentText")
+                                                                       % ("cols" -> "2") 
+                                                					   % ("rows" -> "2")
+                                                                       % ("style" -> "resize: none; width:100%; height:30px;"),
+    "submit" ->  SHtml.ajaxSubmit("Submit", blargh)
+    )
+    
+    ajaxForm(blub)
+  }
 }
