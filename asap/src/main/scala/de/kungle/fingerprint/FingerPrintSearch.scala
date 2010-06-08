9c49e833ee9459dc4c16c37c96d81e5555628c9b
Yousry Abdallah <mail@yousry.de>
2010-06-08 08:06:55 +0200

    bugfix

--- asap/src/main/scala/de/kungle/fingerprint/FingerPrintSearch.scala
+++ asap/src/main/scala/de/kungle/fingerprint/FingerPrintSearch.scala
@@ -6,13 +6,13 @@ class FingerPrintSearch(search: List[String]) {
   
   def this(s: String*) = this(s.toList)
   
-  val MAX_TERMS = 5
+  val MAX_TERMS = 10
     
   val searchTerms = search.take(MAX_TERMS)
   val searchTermsFP = searchTerms.map(x => new ShortTermFingerPrint(x))
   
   val neighbours = FingerPrintSearch.waves.
-    map(w => (w,searchTermsFP.foldLeft(0d)((s, st) => s + st.distance(new ShortTermFingerPrint(w.title_english))))).
+    map(w => (w._1,searchTermsFP.foldLeft(0d)((s, st) => s + st.distance(w._2)))).
     sort((x,y) => x._2 <= y._2).
     take(MAX_TERMS). 
     map(x => x._1)
@@ -24,12 +24,12 @@ object FingerPrintSearch {
   val MAX_SEARCHFIELD = 10000
   val UPDATE_INTERVALL = 1000 * 60 * 10
   
-  var _waves = Wave.findAll(OrderBy(Wave.id, Descending), MaxRows(MAX_SEARCHFIELD))
+  var _waves = Wave.findAll(OrderBy(Wave.id, Descending), MaxRows(MAX_SEARCHFIELD)).map(x => (x.id, new ShortTermFingerPrint(x.title_english)))
   var _lastUpdate = new java.util.Date
   
   def  waves =  synchronized (if((new java.util.Date).getTime <= _lastUpdate.getTime + UPDATE_INTERVALL) _waves else {
     _lastUpdate = new java.util.Date
-    _waves = Wave.findAll(OrderBy(Wave.id, Descending), MaxRows(MAX_SEARCHFIELD))
+    _waves = Wave.findAll(OrderBy(Wave.id, Descending), MaxRows(MAX_SEARCHFIELD)).map(x => (x.id, new ShortTermFingerPrint(x.title_english)))
    _waves
   })
   
