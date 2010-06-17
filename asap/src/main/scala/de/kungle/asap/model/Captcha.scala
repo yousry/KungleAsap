82f9de8af13d8ea35ed6dc0fcebf1c0f1049fa73
Yousry Abdallah <mail@yousry.de>
2010-06-17 06:49:58 +0200

    small modell changes

--- asap/src/main/scala/de/kungle/asap/model/Captcha.scala
+++ asap/src/main/scala/de/kungle/asap/model/Captcha.scala
@@ -28,9 +28,25 @@ class Captcha extends LongKeyedMapper[Captcha] with IdPK {
   object IsSolved extends MappedBoolean(this) {
     override def defaultValue = false
   }
+    
+  object Alternative_A extends MappedPoliteString(this, 60) {
+    override def defaultValue = ""
+  }
+
+  object Alternative_B extends MappedPoliteString(this, 60) {
+    override def defaultValue = ""
+  }
+   
+  object Alternative_C extends MappedPoliteString(this, 60) {
+    override def defaultValue = ""
+  }
+      
+  object Alternative_D extends MappedPoliteString(this, 60) {
+    override def defaultValue = ""
+  }
   
 }
 
 object Captcha extends Captcha with LongKeyedMetaMapper[Captcha] {
-  override def fieldOrder = List(id, Question, Sentence_A, Sentence_B, Sentence_C, IsSolved)
+  override def fieldOrder = List(id, Question, Sentence_A, Sentence_B, Sentence_C, IsSolved, Alternative_A, Alternative_B, Alternative_C, Alternative_D)
 }
