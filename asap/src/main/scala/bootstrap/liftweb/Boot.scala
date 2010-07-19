113f097c16015fcf241ea6226f23efd64b435825
Yousry Abdallah <mail@yousry.de>
2010-07-19 09:41:46 +0200

    small gfx fixes editor disabled

--- asap/src/main/scala/bootstrap/liftweb/Boot.scala
+++ asap/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -47,7 +47,8 @@ class Boot {
     // Build SiteMap
     val entries = 
       Menu(Loc("Home", List("index"), S.??("Home"), LocGroup("default"))) :: 
-//    Menu(Loc("captchatest", List("captchatest"), "Captcha Demo", LocGroup("default"))) :: 
+//    Menu(Loc("captchatest", List("captchatest"), "Captcha Demo", LocGroup("default"))) ::
+      Menu(Loc("piclist", List("piclist"), "POTD", LocGroup("default"))) ::
       Menu(Loc("Control",("admin" :: "control" :: Nil) -> false, S.??("Control"), LocGroup("default"), loggedIn ))  ::
       Nil
     
