d8b7e7276713a0574c328113ba5964400742ba32
Yousry Abdallah <mail@yousry.de>
2010-07-22 13:16:33 +0200

    typo

--- asap/src/main/scala/bootstrap/liftweb/Boot.scala
+++ asap/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -48,7 +48,7 @@ class Boot {
     val entries = 
       Menu(Loc("Home", List("index"), S.??("Home"), LocGroup("default"))) :: 
 //    Menu(Loc("captchatest", List("captchatest"), "Captcha Demo", LocGroup("default"))) ::
-      Menu(Loc("piclist", List("piclist"), "Picture Of The Day", LocGroup("default"))) ::
+      Menu(Loc("piclist", List("piclist"), "Pictures Of The Day", LocGroup("default"))) ::
       Menu(Loc("Control",("admin" :: "control" :: Nil) -> false, S.??("Control"), LocGroup("default"), loggedIn ))  ::
       Nil
     
